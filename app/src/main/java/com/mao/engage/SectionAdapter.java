083f7b3e8e6e1204338406c83e083d66ffaf800c
Lucy Liu <masssb1@gmail.com>
2019-03-22 23:43:02 -0700

    set up for recycler view, working on getting information from db

---
+++ app/src/main/java/com/mao/engage/SectionAdapter.java
@@ -0,0 +1,54 @@
+package com.mao.engage;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
+
+public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.MyViewHolder> {
+    private List<SectionSesh> sectionSeshList;
+
+    /*
+    section buttons referenced button design from section_list_row
+     */
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public Button section;
+
+        public MyViewHolder(View view) {
+            super(view);
+            section = (Button) view.findViewById(R.id.sectionBtn);
+        }
+    }
+
+    /*
+    constructs an adapter based on the section list passed in -- pass db
+    through TeacherResumeActivity
+     */
+    public SectionAdapter(List<SectionSesh> sectionSeshList) {
+        this.sectionSeshList = sectionSeshList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.section_list_row, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    /*
+    sets button to have the section_id of the section
+     */
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        SectionSesh section = sectionSeshList.get(position);
+        holder.section.setText(section.getSection_id());
+    }
+
+    @Override
+    public int getItemCount() {
+        return sectionSeshList.size();
+    }
+}
--- app/src/main/java/com/mao/engage/TeacherResumeActivity.java
+++ app/src/main/java/com/mao/engage/TeacherResumeActivity.java
@@ -15,18 +15,23 @@ public class TeacherResumeActivity extends AppCompatActivity {
 
     private ImageButton backBtn;
     private RecyclerView recyclerView;
-    private MoviesAdapter mAdapter;
-    private List<Movie> movieList = new ArrayList<>();
+    private SectionAdapter mAdapter;
+    private List<SectionSesh> sectionSeshList = new ArrayList<>();
     private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //UI: Recycler view with Choose Your Section on top
         setContentView(R.layout.activity_teacher_resume);
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
+        //backbutton function
         backBtn = findViewById(R.id.teacherBackBtn);
-
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,16 +39,17 @@ public class TeacherResumeActivity extends AppCompatActivity {
             }
         });
 
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new MoviesAdapter(movieList);
+        //create Adapter that access firebase section data based teacher and display as buttons
+        mAdapter = new SectionAdapter(sectionSeshList);
         recyclerView.setAdapter(mAdapter);
 
-        for (int i = 0; i < 10; i++) {
-            Movie movie = new Movie("" + i);
-            movieList.add(movie);
-            System.out.println(i);
-        }
+//        for (int i = 0; i < 10; i++) {
+//            SectionSesh movie = new SectionSesh("1", "2",
+//                    String ta_key, String section_id,
+//                    String ref_key, int magic_key,
+//            ArrayList<String> user_ids);
+//            movieList.add(movie);
+//            System.out.println(i);
+//        }
     }
 }
