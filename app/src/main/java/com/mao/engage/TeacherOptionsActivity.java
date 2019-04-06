bc66cef0f3e7e35aa0b00788df9ee197dd02b13f
michelle54725 <michelle54725@gmail.com>
2019-04-06 12:39:08 -0700

    back buttons all work

--- app/src/main/java/com/mao/engage/TeacherOptionsActivity.java
+++ app/src/main/java/com/mao/engage/TeacherOptionsActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TeacherOptionsActivity extends AppCompatActivity {
 
+    ImageButton backBtn;
     Button createButton;
     Button resumeButton;
     TextView helloText;
@@ -21,10 +23,18 @@ public class TeacherOptionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teacher_options);
 
         name = getIntent().getStringExtra("name");
+        backBtn = findViewById(R.id.backBtn);
         createButton = findViewById(R.id.createNewBtn);
         resumeButton = findViewById(R.id.resumeBtn);
         helloText = findViewById(R.id.helloText3);
         helloText.setText(String.format("Hi, %s", name));
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
