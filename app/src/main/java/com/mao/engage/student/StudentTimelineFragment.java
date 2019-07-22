f6419b05183e2f9f1612137153d09cc31df85ac2
Paul Shao <paulshaoyuqiao1@berkeley.edu>
2019-07-21 20:06:28 -0700

    added PNG export functionality

--- app/src/main/java/com/mao/engage/student/StudentTimelineFragment.java
+++ app/src/main/java/com/mao/engage/student/StudentTimelineFragment.java
@@ -11,11 +11,13 @@
  */
 package com.mao.engage.student;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
@@ -23,6 +25,8 @@ import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
@@ -30,9 +34,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LimitLine;
@@ -41,13 +49,16 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.mao.engage.FirebaseUtils;
 import com.mao.engage.R;
 import com.mao.engage.teacher.TeacherCreateClassActivity;
 import com.mao.engage.teacherclassactivity.TeacherClassActivity;
 import com.mao.engage.teacherclassactivity.TimelineDataRetrieval;
+import com.rey.material.widget.FloatingActionButton;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -86,8 +97,13 @@ public class StudentTimelineFragment extends Fragment {
     private ConstraintLayout circleWrapper;
     private TextView engagedCountText;
 
+    private FloatingActionButton exportImageButton;
+
     TimerTask retrieveDataTask;
     private TimelineDataRetrieval dataRetrieval;
+
+    private boolean isEndOfSection = false;
+    private String exportDialogTitle = "";
 
     public StudentTimelineFragment() {
         // required empty public constructor
@@ -107,6 +123,7 @@ public class StudentTimelineFragment extends Fragment {
         endTimeText = view.findViewById(R.id.endTimeText);
         circleWrapper = view.findViewById(R.id.circleWrapper);
         engagedCountText = view.findViewById(R.id.engagedCount);
+        exportImageButton = view.findViewById(R.id.exportActionButton);
         activity = (StudentClassActivity) getActivity();
 
         //initializes lists of slider values from activity
@@ -133,40 +150,26 @@ public class StudentTimelineFragment extends Fragment {
                     return;
                 }
                 //run on separate Ui thread to no conflict other threads
-                myActivity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() { retrieveData();}
-                });
+                myActivity.runOnUiThread(() -> useDummyData()); // FIXME: Replace with retrieve Data; currently on dummy data for DEMO purposes
             }
         };
         //retrieves data every 5000 ms (5s)
         Timer t = new Timer();
-        t.scheduleAtFixedRate(retrieveDataTask, 0, 5000);
+        t.scheduleAtFixedRate(retrieveDataTask, 0, 1000);
         if (FirebaseUtils.compareTime(activity.getEndTime())) {
             Log.d("TEST", "compare: stop retrieve data upon reach time");
             t.cancel();
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            builder.setTitle("Section has ended!");
-            builder.setMessage("Would you like to save your graph?");
-            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                    Log.d("TEST", "selected no save");
-                    FirebaseUtils.removeSection(FirebaseUtils.getMySection());
-                }
-            });
-            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    takeScreenshot();
-                    Log.d("TEST", "selected save graph");
-                    FirebaseUtils.removeSection(FirebaseUtils.getMySection());
-                }
-            });
-            builder.show();
+            isEndOfSection = true;
+            exportDialogTitle = "Time is up! Export Your Image as an PNG to Save to Gallery";
+            showExportImageDialog();
         }
+
+        exportImageButton.setOnClickListener(floatingButton -> {
+            isEndOfSection = false;
+            exportDialogTitle = "Export Your Image as an PNG to Save to Gallery";
+            Log.d("P-TEST", "Export Button Clicked");
+            showExportImageDialog();
+        });
 
         startTime = activity.getStartTime();
         endTime = activity.getEndTime();
@@ -178,43 +181,59 @@ public class StudentTimelineFragment extends Fragment {
         return view;
     }
 
-    private void takeScreenshot() {
-        Date now = new Date();
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+    private boolean checkIfAlreadyhavePermission() {
+        int writeResult = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readResut = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        return writeResult == PackageManager.PERMISSION_GRANTED && readResut == PackageManager.PERMISSION_GRANTED;
+    }
 
-        try {
-            // image naming and path  to include sd card  appending name you choose for file
-            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
-
-            // create bitmap screen capture
-            View v1 = getActivity().getWindow().getDecorView().getRootView();
-            v1.setDrawingCacheEnabled(true);
-            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
-            v1.setDrawingCacheEnabled(false);
-            Log.d("TEST", "takeScreenshot");
-            File imageFile = new File(mPath);
-
-            FileOutputStream outputStream = new FileOutputStream(imageFile);
-            int quality = 100;
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
-            outputStream.flush();
-            outputStream.close();
-
-            openScreenshot(imageFile);
-        } catch (Throwable e) {
-            // Several error may come out with file handling or DOM
-            e.printStackTrace();
+    private void showExportImageDialog() {
+        if (checkIfAlreadyhavePermission()) {
+            // If request is cancelled, the result arrays are empty.
+            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+            builder.setTitle(exportDialogTitle);
+            builder.setMessage("To save your graph, please enter a name below for the graph:");
+            final EditText imageNameInput = new EditText(getContext());
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+            );
+            layoutParams.leftMargin = 8;
+            layoutParams.rightMargin = 8;
+            imageNameInput.setHint("Please enter a non-empty name for your file");
+            imageNameInput.setLayoutParams(layoutParams);
+            builder.setView(imageNameInput);
+            builder.setNegativeButton("Cancel", (dialog, which) -> {
+                dialog.cancel();
+                Log.d("TEST", "selected no save");
+                FirebaseUtils.removeSection(FirebaseUtils.getMySection());
+            });
+            builder.setPositiveButton("Confirm", (dialog, which) -> {
+                dialog.dismiss();
+                String imageName = imageNameInput.getText().toString();
+                if (imageName.trim().length() == 0) {
+                    Toast.makeText(getContext(), "Please enter a non-empty name", Toast.LENGTH_LONG).show();
+                } else {
+                    boolean success = chart.saveToGallery(imageName);
+                    Log.d("TEST", "selected save graph");
+                    if (success) {
+                        Toast.makeText(getContext(), "Image has been saved successfully!", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getContext(), "Image saving has failed, please try again", Toast.LENGTH_LONG).show();
+                    }
+                    if (isEndOfSection) {
+                        FirebaseUtils.removeSection(FirebaseUtils.getMySection());
+                    }
+                }
+            });
+            builder.show();
+        } else {
+            ActivityCompat.requestPermissions(this.activity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
         }
     }
 
-    private void openScreenshot(File imageFile) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        Uri uri = Uri.fromFile(imageFile);
-        intent.setDataAndType(uri, "image/*");
-        Log.d("TEST", "openScreenshot");
-        startActivity(intent);
-    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -223,6 +242,47 @@ public class StudentTimelineFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private void useDummyData() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 4));
+        entries.add(new Entry(1, 1));
+        entries.add(new Entry(2, 2));
+        entries.add(new Entry(3, 4));
+
+        LineDataSet dataSet = new LineDataSet(entries, "You");
+        dataSet.setColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        dataSet.setValueTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+        //****
+        // Controlling X axis
+        XAxis xAxis = chart.getXAxis();
+        // Set the xAxis position to bottom. Default is top
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //Customizing x axis value
+        final String[] months = new String[]{"7:00", "7:15", "7:30", "7:45"};
+
+        IAxisValueFormatter formatter = (value, axis) -> months[(int) value];
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+
+        //***
+        // Controlling right side of y axis
+        YAxis yAxisRight = chart.getAxisRight();
+        yAxisRight.setEnabled(false);
+
+        //***
+        // Controlling left side of y axis
+        YAxis yAxisLeft = chart.getAxisLeft();
+        yAxisLeft.setGranularity(1f);
+
+        // Setting Data
+        LineData data = new LineData(dataSet);
+        chart.setData(data);
+        //refresh
+        chart.invalidate();
+        //
     }
 
     private void retrieveData() {
@@ -236,6 +296,11 @@ public class StudentTimelineFragment extends Fragment {
         //adds user slider value to ArrayList of entries
         int mySliderValue = dataRetrieval.getMySliderValue(FirebaseUtils.getPsuedoUniqueID());
         meValues.add(new Entry(index, mySliderValue));
+        // dummy data
+        int[][] dummyData = {{1, 10}, {2, 50}, {3, 70}, {4, 50}, {5, 90}};
+        for (int[] data : dummyData) {
+            meValues.add(new Entry(data[0], data[1]));
+        }
         Log.d("TEST", "Me value: " + mySliderValue);
 
         //adding class average slider value to ArrayList of entries
@@ -314,7 +379,9 @@ public class StudentTimelineFragment extends Fragment {
 
         //inputs sets into data form that can be graphed
         lineData = new LineData(meSet, classSet);
+
         chart.setData(lineData);
+
 
         //notifies that data has changed
         //tells the graph to update
@@ -370,7 +437,7 @@ public class StudentTimelineFragment extends Fragment {
         Log.d("TEST", "end of graphData");
 
         //redraws
-        chart.invalidate();
+//        chart.invalidate();
     }
 
     //All adjustments for engaged count (number of students in a section)
